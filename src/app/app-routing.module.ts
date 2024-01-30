dc28248f12e25746deb0244d33c6089319f6cd53
unknown <pothalatriveni376@gmail.com>
2024-01-30 10:59:44 +0530

    triveni update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { LimitsComponent } from './limits/limits.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { OffersComponent } from './offers/offers.component';
+
+
+const routes: Routes = [
+  {path:'navbar',component:NavbarComponent,
+    children:[
+      {path:'dashboard',component:DashboardComponent},
+      {path:'categories',component:CategoriesComponent},
+      {path:'limits',component:LimitsComponent},
+      {path:'expense',component:ExpenseComponent},
+      {path:'offers',component:OffersComponent},
+      
+    
+    ]
+    
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
